61609e22585cd09859d9c719f8adda4162c44806
masa24 <masadong18@email.com~>
2024-10-13 04:37:45 +0000

    edit on practice_run.py

--- practice_run.py
+++ practice_run.py
@@ -1,34 +1,64 @@
 #!/user/bin/env python3
+import asyncio
 from mavsdk import System
-'''
-Create Take Off function
-'''
+from mavsdk.action import OrbitYawBehavior
 
-'''
-Create Landing Function
-'''
-
-#loitering function
-async def loiter_in_rotation(drone, position):
-    
-
-# main running function
 async def run():
-
-    #Init drone
     drone = System()
     await drone.connect(system_address="udp://:14540")
+
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print("Drone discovered!")
+            break
+
+    print("Waiting for drone to have a global position estimate...")
+    async for health in drone.telemetry.health():
+        if health.is_global_position_ok and health.is_home_position_ok:
+            print("-- Global position estimate OK")
+            break
+
+    position = await drone.telemetry.position().__aiter__().__anext__()
+    orbit_height = position.absolute_altitude_m+10
+    yaw_behavior = OrbitYawBehavior.HOLD_FRONT_TO_CIRCLE_CENTER
 
     print("-- Arming")
     await drone.action.arm()
 
-    print("-- Taking off")
+    take_off()
+    orbit(drone, orbit_height, yaw_behavior)
+    landing()
+
+'''
+Create Take Off function
+'''
+async def take_off():
+    print("--- Taking Off")
     await drone.action.takeoff()
-
     await asyncio.sleep(10)
+'''
+Create Landing Function
+'''
+async def landing():
+    await drone.action.return_to_launch()
+    print("--- Landing")
 
-    print("-- Landing")
-    await drone.aciton.land()
+'''
+Create Loitering Function
+'''
+async def orbit(drone, orbit_height, yaw_behavior):
+    print('Do orbit at 10m height from the ground')
+    await drone.action.do_orbit(
+        radius_m=10,
+        velocity_ms=2,
+        yaw_behavior=yaw_behavior,
+        latitude_deg=47.398036222362471,
+        longitude_deg=8.5450146439425509,
+        absolute_altitude_m=orbit_height
+    )
+    await asyncio.sleep(60)
+
 
 if __name__ == "__main__":
     asyncio.run(run())
